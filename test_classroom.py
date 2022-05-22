eeddf5ffbcc457e08bfa354c6d395cf6afcc41cf
yosko123 <yosif.g.saltiel.2020@elsys-bg.org>
2022-05-22 23:55:09 +0300

    Update test_classroom.py

--- test_classroom.py
+++ test_classroom.py
@@ -1,4 +1,8 @@
+# I can't test since all parts are real-time.
+
 import unittest as ut
+
+from click import pass_context
 from Events import Classroom
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,30 +13,25 @@ from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly',
           'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
           'https://www.googleapis.com/auth/calendar']
-
+creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+service = build("calendar", 'v3', credentials=creds)
+service = build("classroom", 'v1', credentials=creds)
 class TestClassroom(ut.TestCase):
     def test_get_course(self): 
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        service = build("classroom", 'v1', credentials=creds)
         classroom = Classroom(service)
         self.assertTrue(classroom.get_course(course_id=514348985853))
         
     def test_list_course(self): 
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        service = build("classroom", 'v1', credentials=creds)
         classroom = Classroom(service, creds=creds)
         self.assertTrue(classroom.list_courses())
     
     def test_get_assignment_from_course(self):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        service = build("classroom", 'v1', credentials=creds)
         classroom = Classroom(service, creds=creds)
         self.assertTrue(classroom.get_assignments_from_course(course_id=514348985853))
     
-    def test_list_assignments(self): 
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        service = build("classroom", 'v1', credentials=creds)
+    def test_list_assignments(self):
         classroom = Classroom(service, creds=creds)
+        self.assertTrue(classroom.list_assignments())
 
 class TestCalendar(ut.TestCase): 
     def test_get_events(self): 
